8c264c94e658c85cf7e42a574466b940cce9ccf2
Brant Knudson <brant.knudson@hpe.com>
2021-06-10 15:57:15 -0500

    CASMPET-4249: Clean up old backups
    After the new backup is made, any older backups are removed.
    This is to prevent the disk from filling up.

--- test_postgres_db_backup.py
+++ test_postgres_db_backup.py
@@ -69,8 +69,141 @@ def test_calc_key_base(m_datetime):
     assert res == exp
 
 
+def test_get_keys_for_db():
+    # get_keys_for_db calls list_objects with the bucket and prefix.
+    # This returns a dict with `Contents` set to a list of dicts (one for each
+    # object in the bucket that matches the prefix) that has a
+    # `Key`. get_keys_for_db returns all the Key values as a list.
+    stg_client = mock.Mock()
+    stg_client.list_objects.return_value = {
+        'Contents': [
+            {'Key': 'key1'},
+            {'Key': 'key2'},
+        ],
+    }
+    db_name = 'fake_db_name'
+    bucket = 'fake_bucket'
+    res = postgres_db_backup.get_keys_for_db(stg_client, bucket, db_name)
+    exp_res = ['key1', 'key2', ]
+    assert res == exp_res
+
+    stg_client.list_objects.assert_called_once_with(
+        Bucket=bucket, Prefix=f'{db_name}-')
+
+
+def test_calc_timestamp():
+    # calc_timestamp() extracts the timestamp from a key. The key is like
+    # <db_name>-<timestamp>.<suffix>.
+    db_name = 'fake_db_name'
+    fake_timestamp = '2021-06-10T15:07:42'
+    key = f'{db_name}-{fake_timestamp}.manifest'
+    res = postgres_db_backup.calc_timestamp(db_name, key)
+    assert res == fake_timestamp
+
+
+def test_calc_timestamps():
+    # calc_timestamps takes a list of keys and returns a sorted list of
+    # unique timestamps from the keys.
+    db_name = 'fake_db_name'
+    fake_timestamp_1 = '2021-06-09T15:07:42'
+    fake_timestamp_2 = '2021-06-10T18:11:10'
+    # Keys are out of timestamp order here, calc_timestamps sorts the results.
+    keys = [
+        f'{db_name}-{fake_timestamp_2}.manifest',
+        f'{db_name}-{fake_timestamp_2}.psql',
+        f'{db_name}-{fake_timestamp_1}.manifest',
+        f'{db_name}-{fake_timestamp_1}.psql',
+    ]
+    res = postgres_db_backup.calc_timestamps(db_name, keys)
+    exp_res = [fake_timestamp_1, fake_timestamp_2]  # sorted!
+    assert res == exp_res
+
+
+@mock.patch('postgres_db_backup.get_keys_for_db', autospec=True)
+@mock.patch('postgres_db_backup.calc_timestamps', autospec=True)
+def test_get_backup_timestamps(m_cts, m_gkd):
+    stg_client = mock.Mock()
+    bucket = 'fake_bucket'
+    db_name = 'fake_db_name'
+    (res_keys, res_timestamps) = postgres_db_backup.get_backup_timestamps(
+        stg_client, bucket, db_name)
+    m_gkd.assert_called_once_with(stg_client, bucket, db_name)
+    m_cts.assert_called_once_with(db_name, m_gkd.return_value)
+    assert res_keys is m_gkd.return_value
+    assert res_timestamps is m_cts.return_value
+
+
+def test_calc_timestamps_to_delete_empty():
+    # When there are no timestamps, there's none to delete.
+    backup_timestamps = []
+    res = postgres_db_backup.calc_timestamps_to_delete(backup_timestamps)
+    assert res == []
+
+
+def test_calc_timestamps_to_delete_one():
+    # When there's 1 timestamp, there's none to delete.
+    backup_timestamps = ['ts1']
+    res = postgres_db_backup.calc_timestamps_to_delete(backup_timestamps)
+    assert res == []
+
+
+def test_calc_timestamps_to_delete_two():
+    # When there's 2 timestamps, there's 1 to delete.
+    backup_timestamps = ['ts1', 'ts2']
+    res = postgres_db_backup.calc_timestamps_to_delete(backup_timestamps)
+    assert res == ['ts1']
+
+
+def test_calc_timestamps_to_delete():
+    # When there's 3 timestamps, there's 2 to delete.
+    backup_timestamps = ['ts1', 'ts2', 'ts3']
+    res = postgres_db_backup.calc_timestamps_to_delete(backup_timestamps)
+    assert res == ['ts1', 'ts2']
+
+
+def test_delete_backups():
+    stg_client = mock.Mock()
+    bucket = 'fake_bucket'
+    db_name = 'fake_db_name'
+    fake_timestamp_1 = '2021-06-09T15:07:42'
+    fake_timestamp_2 = '2021-06-10T18:11:10'
+    # Keys are out of timestamp order here, calc_timestamps sorts the results.
+    keys = [
+        f'{db_name}-{fake_timestamp_2}.manifest',
+        f'{db_name}-{fake_timestamp_2}.psql',
+        f'{db_name}-{fake_timestamp_1}.manifest',
+        f'{db_name}-{fake_timestamp_1}.psql',
+    ]
+    timestamps_to_delete = [fake_timestamp_1]
+    postgres_db_backup.delete_backups(
+        stg_client, bucket, db_name, keys, timestamps_to_delete
+    )
+
+    # The keys with fake_timestamp_1 were deleted.
+    stg_client.delete_object.assert_any_call(
+        Bucket=bucket, Key=f'{db_name}-{fake_timestamp_1}.manifest')
+    stg_client.delete_object.assert_any_call(
+        Bucket=bucket, Key=f'{db_name}-{fake_timestamp_1}.psql')
+    assert stg_client.delete_object.call_count == 2
+
+
+@mock.patch('postgres_db_backup.get_backup_timestamps', autospec=True)
+@mock.patch('postgres_db_backup.calc_timestamps_to_delete', autospec=True)
+@mock.patch('postgres_db_backup.delete_backups', autospec=True)
+def test_cleanup_old_backups(m_db, m_cttd, m_gbt):
+    m_gbt.return_value = (mock.sentinel.keys, mock.sentinel.backup_timestamps)
+    stg_client = mock.Mock()
+    bucket = 'fake_bucket'
+    db_name = 'fake_db_name'
+    postgres_db_backup.cleanup_old_backups(stg_client, bucket, db_name)
+    m_gbt.assert_called_once_with(stg_client, bucket, db_name)
+    m_cttd.assert_called_once_with(mock.sentinel.backup_timestamps)
+    m_db.assert_called_once_with(
+        stg_client, bucket, db_name, mock.sentinel.keys, m_cttd.return_value)
+
+
 @mock.patch('postgres_db_backup.kubernetes.config')
-@mock.patch('postgres_db_backup.postgres_db_backup')
+@mock.patch('postgres_db_backup.postgres_db_backup', autospec=True)
 def test_main(mock_pdb, mock_ks_config, monkeypatch):
     db_name = 'fake_db_name'
     monkeypatch.setenv('DB_NAME', db_name)
